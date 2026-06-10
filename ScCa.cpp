9afe692a4f730092be220ea901e0e1435b126560
Cicol421 <luccascarfosuttin14@hotmail.com>
2026-06-09 21:43:11 -0500

    Version: 1

--- ScCa.cpp
+++ ScCa.cpp
@@ -1,24 +1,36 @@
 #include <iostream>
+#include <cmath>
 
 class Calculator{
     public:
-        int x;
-        int y;
-
-        int add(int x, int y){
+        int add(double x, double y){
             return (x + y);
         }
-        int sub(int x, int y){
+        int sub(double x, double y){
             return (x - y);
         }
-        int mul(int x, int y){
+        int mul(double x, double y){
             return (x * y);
         }
-        int div(int x, int y){
+        int div(double x, double y){
             if(x == 0){
                 return 0;
             } else if(y == 0){
                 return -1;
+            } else{
+                return (x / y);
+            }
+        }
+        int pow(double x, double y){
+            return (pow(x, y));
+        }
+        int roo(double x, double y){
+            if(y == 2){
+                return (sqrt(x));
+            } else if(y == 3){
+                return (cbrt(x));
+            } else{
+                return (pow(x, (1.0 / y)));
             }
         }
 };
